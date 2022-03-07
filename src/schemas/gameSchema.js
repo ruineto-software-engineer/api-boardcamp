14a78f9bff60da9cc43361d258ea26dd0c89eb44
Rui Neto <ruineto11@gmail.com>
2022-03-07 18:13:47 -0300

    fix: game schema validation url

--- src/schemas/gameSchema.js
+++ src/schemas/gameSchema.js
@@ -2,7 +2,7 @@ import joi from 'joi';
 
 const gameSchema = joi.object({
   name: joi.string().required(),
-  image: joi.string().required(),
+  image: joi.string().uri().required(),
   stockTotal: joi.string().required(),
   categoryId: joi.number().integer().required(),
   pricePerDay: joi.string().required()

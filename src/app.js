e9d88709c5c0ee28fe3f79bef871843891bac3e8
Rui Neto <ruineto11@gmail.com>
2022-04-12 02:13:27 -0300

    Prepare for deploy

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import express, { json } from 'express';
 import cors from 'cors';
 import router from './routes/index.js';
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+
 app.use(cors());
 app.use(json());
-
 app.use(router);
 
-app.listen(4000, () => {
-  console.log("Listening on 4000");
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on PORT ${process.env.PORT}`);
 })
--- src/db.js
+++ src/db.js
@@ -6,6 +6,9 @@ const { Pool } = pkg;
 
 const connection = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default connection;
